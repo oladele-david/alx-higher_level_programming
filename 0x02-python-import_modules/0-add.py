47978dacb00fdd2acb30ab299fd9a4d23fd31f7f
oladele-david <tdboy59@gmail.com>
2024-01-04 14:25:45 -0800

    fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-add_0 = __import__("add_0")
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ = "__main__":
+    from add_0 import add
+
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
