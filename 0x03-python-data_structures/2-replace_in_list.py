e64defe932401576a8823438a7022255d5b57425
oladele.david <tdboy59@gmail.com>
2024-01-08 14:12:58 +0100

    Task 2 commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] == idx:
+                my_list[i] = element
+
+        return my_list
+
