6695660c382b7c0c3ab46c7446c62b88f42d1593
oladele.david <tdboy59@gmail.com>
2024-01-08 15:13:40 +0100

    Task 8 commit 5

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     my_turple = ()
-    if sentence is None:
-        return None
+    if len(sentence) == 0:
+        my_turple = 0, "None"
     else:
         my_turple = len(sentence), sentence[0]
     return my_turple
