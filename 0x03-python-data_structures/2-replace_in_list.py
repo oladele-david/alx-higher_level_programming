e6ff82c5619433abb6655abc01038458ae645ae1
oladele.david <tdboy59@gmail.com>
2024-01-08 14:18:14 +0100

    Task 2 commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        for i in range(len(my_list)):
-            if my_list[i] == idx:
-                my_list[i] = element
-
+        my_list[idx] = element
         return my_list
-
