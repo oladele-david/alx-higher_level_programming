0b8ab5bb0d7061430b7725862c1b17bf9ad66a54
oladele.david <tdboy59@gmail.com>
2024-01-12 00:21:53 +0100

    Task 1 commit 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    # Create a new list to store the modified elements
-    new_list = [replace if element == search else element for element in my_list]
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
     return new_list
