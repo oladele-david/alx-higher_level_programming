75cc84992570eac4504947c014fd8fb636d72f0a
oladele.david <tdboy59@gmail.com>
2024-01-11 23:53:38 +0100

    Task 0 commit 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same size as the input matrix
     result_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
