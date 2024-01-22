493f09ec3fba2eef71ba3418a840626076536aa6
oladele.david <tdboy59@gmail.com>
2024-01-23 00:50:27 +0100

    Initial commit: Task  1 fix 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,6 +10,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError:
+    except (TypeError, ValueError):
         print(value)
         return (False)
