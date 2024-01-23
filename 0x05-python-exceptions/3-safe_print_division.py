09bd9095de692f7e123ac13bb86177203ef46aa7
oladele.david <tdboy59@gmail.com>
2024-01-23 01:51:34 +0100

    Initial commit: Task  3 fixed

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         answer = None
     finally:
-        return ("Inside result: {}".format(answer))
-    return (answer)
+        print("Inside result: {}".format(answer))
+    return answer
