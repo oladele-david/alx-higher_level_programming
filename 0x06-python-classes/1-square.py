c6cc92d11b9b1b9884153a511dac9a85bda77b7c
oladele.david <tdboy59@gmail.com>
2024-01-23 18:22:40 +0100

    Initial commit: Task  1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" A class that defines a Square class """
+
+
+class Square(object):
+    """ A class that define a Square """
+
+    def __init__(self, size):
+        """ Initialize Private instance"""
+        self.__size = size
