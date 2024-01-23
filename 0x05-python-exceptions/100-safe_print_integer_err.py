b1b7ca6cf8a1bede859a7dc0cd6f40688bff409a
oladele.david <tdboy59@gmail.com>
2024-01-23 02:38:28 +0100

    Initial commit: Task  100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        integer_value = int(value)
+        print("{:d}".format(integer_value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
